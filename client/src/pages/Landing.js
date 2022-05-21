4f56ad72fdaebe0559474adbd7edd5b96880c6df
ThameeraFernando <sithnuwan111@gmail.com>
2022-05-21 15:27:22 +0530

    complete landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,13 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Logo from "../assets/images/logo.svg";
-import Main from "../assets/images/main.svg";
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import Logo from "../assets/images/logo.svg";
+// import Main from "../assets/images/main.svg";
 
+// const Landing = () => {
+//   return (
+//     <Link to="/register" className="btn btn-hero">
+//       login-register
+//     </Link>
+//   );
+// };
+
+// export default Landing;
+
+import React from "react";
+import main from "../assets/images/project.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
 const Landing = () => {
   return (
-    <Link to="/register" className="btn btn-hero">
-      login-register
-    </Link>
+    <Wrapper>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Project <span>Manager</span> App
+          </h1>
+          <p>
+            Educated during the grip of the COVID-19 pandemic, the class of 2021
+            concludes an unusual academic experience only to face an unsteady
+            global economy. Ng’s book sets out to help these job seekers—a group
+            that’s often clueless even in the best of times—get started on the
+            right foot.
+          </p>
+          <Link to="/register">
+            <button className="btn btn-hero">login/register</button>
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
   );
 };
 
